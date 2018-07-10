e8221708dd026b829661afe2b0ecfa3c07b42632
colton <colton.morris@tcnbroadcasting.com>
2018-07-09 23:20:57 -0600

    cheers

--- projects/xml-to-sheets/README.md
+++ projects/xml-to-sheets/README.md
@@ -1,6 +1,6 @@
 # XML to Sheets
 
-Converts an XML file into a google sheets file.
+Converts an XML file into a csv that can then be added to google sheets by using it's gui's import button.
 
 ## To Run
 `bazel run //projects/xml-to-sheets:main`
--- projects/xml-to-sheets/main.js
+++ projects/xml-to-sheets/main.js
@@ -2,22 +2,20 @@ var fs = require('fs')
 var xmldoc = require('xmldoc')
 var GoogleSpreadsheet = require('google-spreadsheet')
 var async = require('async')
+var Promise = require('bluebird')
 
-var sheet_id = require('./mono/projects/xml-to-sheets/config.js')
-var creds = require('./mono/projects/xml-to-sheets/service-account-creds.json')
-
-// spreadsheet key is the long id in the sheets URL
-var doc = new GoogleSpreadsheet('1mTVXpz53ecLVeSkFTSRPYV3MX7misxH1meDoi3E2_Ec');
-var sheet;
+var fields = ["type", "address", "body", "epoch", "utc", "read", "locked"]
+var file = 'projects/xml-to-sheets/output.csv'
 
 
+
+// read input data
 var xml = fs.readFileSync( './projects/xml-to-sheets/input.xml', 'ascii', function(err, xml) {
   return xml
 })
-
 var document = new xmldoc.XmlDocument(xml)
 
-// parse xml into an array
+// parse xml input into an array
 var rows = []
 document.eachChild(function(file) {
   file.eachChild(function(thread) {
@@ -36,38 +34,35 @@ document.eachChild(function(file) {
   })
 })
 
+// convert array into string
+var stringify = fields.join(',') + '\n'
+stringify += rows.map((row) => {
+  return fields.map((field) => {
+    switch(field) {
+      case 'address':
+        return row.address
+      case 'body':
+        return '\"' + row.body.replace(/\"/g, '\'') + '\"'
+      case 'epoch':
+        return row.epoch
+      case 'utc':
+        return row.utc
+      case 'read':
+        return row.read
+      case 'type':
+        return row.type
+      case 'locked':
+        return row.locked
+    }
+  }).join(',')
+}).join('\n')
 
-// save array into spreadsheet
-var count = 0
-async.series([
-  function setAuth(step) {
-    doc.useServiceAccountAuth(creds, step)
-  },
-  function getInfoAndWorksheets(step) {
-    doc.getInfo(function(err, info) {
-      console.log('Loaded doc: ' + info.title + ' by '+ info.author.email)
-      sheet = info.worksheets[0]
-      console.log('sheet rows: ', sheet.rowCount)
-      step()
-    })
-  },
-  function addRows(step) {
-    // TODO do an async map so that each row is saved before starting another
-    rows.map(function(row) {
-      return new Promise(function(resolve, reject) {
-        sheet.addRow(row, function(err, _row) {
-          if (err == null) {
-            console.log('something went wrong: ', row)
-            reject(err)
-          } else {
-            count++
-            resolve(_row)
-          }
-        })
-      })
-    })
-  },
-])
 
-console.log('total rows: ', rows.length)
-console.log('rows added: ', count)
+//save string into file
+fs.writeFile(file, stringify, 'utf8', function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('It\'s saved!');
+  }
+});
