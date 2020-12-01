492ff72e62e1c5e1b3f424568fa7fdaef7395b91
Colton Morris <colton.morris@tcnbroadcasting.com>
2020-12-01 15:46:27 -0700

    updated to latest version of golang and gazelle

--- projects/angular-example/src/main.dev.ts
+++
@@ -1,4 +0,0 @@
-import {platformBrowser} from '@angular/platform-browser';
-import {AppModuleDevNgFactory} from './app.module.dev.ngfactory';
-
-platformBrowser().bootstrapModuleFactory(AppModuleDevNgFactory);
